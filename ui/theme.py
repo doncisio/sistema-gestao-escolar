0b4724a39e64559ac6bb7b017d85f7d67cf0d750
doncisio <tarcisioing39@gmail.com>
2025-11-25 21:05:03 -0300

    Auto-sync: 25/11/2025 21:05

--- ui/theme.py
+++ ui/theme.py
@@ -25,7 +25,7 @@ Colocar as cores aqui facilita a alteração de aparência e a revisão
 durante refactors que movem widgets para `ui/`.
 """
 
-CO_BG = '#003A70'   # cor principal de fundo (co1)
-CO_FG = '#F5F5F5'   # cor de texto (co0)
-CO_ACCENT = '#4A86E8'  # cor de destaque/acao (co4)
-CO_WARN = '#F7B731'  # cor do botão/aviso (co6)
+# CO_BG = '#003A70'   # cor principal de fundo (co1)
+# CO_FG = '#F5F5F5'   # cor de texto (co0)
+# CO_ACCENT = '#4A86E8'  # cor de destaque/acao (co4)
+# CO_WARN = '#F7B731'  # cor do botão/aviso (co6)
